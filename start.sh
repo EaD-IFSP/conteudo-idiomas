fad63f81ac93377c67dfd8bbe92fc7b39668efc3
Paulo José Evaristo da Silva <65752313+pevaristo@users.noreply.github.com>
2024-05-24 17:39:24 -0300

    Update start.sh
    inclusão do overwrite para ganhar tempo na atualização

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ for file in *.h5p
 do
     cp "$file" "../${file%.h5p}.zip"
     mkdir "../${file%.h5p}"
-    unzip "../${file%.h5p}.zip" -d "../${file%.h5p}"
+    unzip -o "../${file%.h5p}.zip" -d "../${file%.h5p}"
     rm "../${file%.h5p}.zip"
     cp template.html "../${file%.h5p}.html"
     sed -i "s/h5p-folder/${file%.h5p}/g" "../${file%.h5p}.html"
